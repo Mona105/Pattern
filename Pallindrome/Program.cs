ee25fb2183a1482a704cdd48996e747dfc4b10ff
ROY <ROY@DESKTOP-26H23PD>
2022-02-20 14:16:10 +0530

    Add project files.

---
+++ Pallindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int i, j, k;
+            for (i = 1; i <= 4; i++)
+            {
+                k = 0;
+                for (j = 1; j <= 4; j++)
+                {
+                    if (j <= i)
+
+                        Console.Write(i + k);
+
+                    else
+                        Console.Write(" ");
+
+
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+    }
+}
